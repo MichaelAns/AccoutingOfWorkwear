fb1ebdb59ae04b0ef1de1e7df5b5f61ed83d374e
Михаил Щукин <mi035ms@gmail.com>
2022-11-29 15:22:44 +0300

    Создание общего абстрактного класса для ViewModel

---
+++ AccoutingOfWorkwear/ViewModels/Base/BaseEntityViewModel.cs
@@ -0,0 +1,64 @@
+﻿using AoW.EntityFramework.Models.Base;
+using MyMVVM.ViewModelBase;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AoW.WPF.ViewModels.Base
+{
+    internal abstract class BaseEntityViewModel<Model> : ViewModel
+        where Model : BaseEntity
+    {
+        public BaseEntityViewModel()
+        {
+            LoadAsync();
+        }
+
+        private List<Model> _models;
+        private Model _selectedItem;
+
+        /// <summary>
+        /// Список всех записей
+        /// </summary>
+        public List<Model> Items
+        {
+            get => _models;
+            set => Set(ref _models, value);
+        }
+
+        /// <summary>
+        /// Выделенная запись из списка
+        /// </summary>
+        public Model SelectedItem
+        {
+            get => _selectedItem;
+            set => Set(ref _selectedItem, value);
+        }
+
+        /// <summary>
+        /// Асинхронная загрузка данных из БД
+        /// </summary>
+        private async void LoadAsync()
+        {
+            Get().ContinueWith(async task =>
+            {
+                if (task.Exception == null)
+                {
+                    Items = (List<Model>)task.Result;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Способ получения данных из БД
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Task<ICollection> Get();
+
+        /// <summary>
+        /// Команда переключения модели представления
+        /// </summary>
+        public ICommand UpdateCurrentViewModelCommand => MainViewModel.Navigator.UpdateCurrentViewModelCommand;
+    }
+}
--- AccoutingOfWorkwear/ViewModels/WorkwearViewModel.cs
+++ AccoutingOfWorkwear/ViewModels/WorkwearViewModel.cs
@@ -1,17 +1,16 @@
-﻿using MyMVVM.ViewModelBase;
+﻿using MyMVVM.Navigation.Navigators;
+using MyMVVM.ViewModelBase;
 using System.Windows.Input;
 
 namespace AoW.WPF.ViewModels
 {
     internal class WorkwearViewModel : ViewModel
     {
-        public ICommand UpdateCurrentViewModelCommand { get; set; }
-        //void fikalis(object obj) => MainViewModel.Navigator.CurrentViewModel = new StaffViewModel();
+        public ICommand UpdateCurrentViewModelCommand => MainViewModel.Navigator.UpdateCurrentViewModelCommand;
 
         public WorkwearViewModel()
         {
-            //UpdateCurrentViewModelCommand = new RelayCommand((obj) => MainViewModel.Navigator.CurrentViewModel = new StaffViewModel(), (obj) => true);
-            
+                        
         }
     }
 }
