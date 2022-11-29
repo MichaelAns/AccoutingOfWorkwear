eda99f20a5b3fbca6f2b328f62a4f3daeb59762c
Михаил Щукин <mi035ms@gmail.com>
2022-11-29 15:22:44 +0300

    Создание общего абстрактного класса для ViewModel

--- AccoutingOfWorkwear/ViewModels/StaffViewModel.cs
+++ AccoutingOfWorkwear/ViewModels/StaffViewModel.cs
@@ -1,21 +1,73 @@
 ﻿using AoW.EntityFramework.Date;
 using AoW.EntityFramework.Models;
-using AoW.WPF.ViewModels.Base;
+using AoW.WPF.Views;
+using MyMVVM.Commands;
+using MyMVVM.ViewModelBase;
 using System.Collections;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AoW.WPF.ViewModels
 {
-    internal class StaffViewModel : BaseEntityViewModel<Staff>
+    public class StaffViewModel : ViewModel
     {
-        protected override async Task<ICollection> Get()
+        private List<Staff> _staff;
+        private Staff _selectedItem;
+        public StaffViewModel()
+        {
+            LoadAsync();
+        }
+
+        /// <summary>
+        /// Список сотрудников
+        /// </summary>
+        public List<Staff> Staff
+        {
+            get => _staff;
+            set => Set(ref _staff, value);
+        }
+
+        /// <summary>
+        /// Выделенный сотрудник
+        /// </summary>
+        public Staff SelectedItem 
+        { 
+            get => _selectedItem;
+            set => Set(ref _selectedItem, value);
+        }
+
+        #region загрузка данных       
+
+        /// <summary>
+        /// Асинхронная загрузка данных из базы данных
+        /// </summary>
+        private async void LoadAsync()
+        {
+            Get().ContinueWith(async task =>
+            {
+                if (task.Exception == null)
+                {
+                    Staff = (List<Staff>)task.Result;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Получение списка сотрудников из базы
+        /// </summary>
+        /// <returns>Список сотрудников</returns>
+        private async Task<ICollection> Get()
         {
             using (var dbContext = new AowDbContextFactory().CreateDbContext())
             {
-                return dbContext.Staff.ToList();
+                return new List<Staff>(dbContext.Staff);
             }
         }
-        
+        #endregion
+                
+        public ICommand UpdateCurrentViewModelCommand => MainViewModel.Navigator.UpdateCurrentViewModelCommand;
+
+
     }
 }
