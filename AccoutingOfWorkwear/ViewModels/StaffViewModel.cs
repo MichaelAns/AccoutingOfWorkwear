eedaae835f038b77389948f105493aa2c13a8f97
Михаил Щукин <mi035ms@gmail.com>
2022-11-29 23:06:21 +0300

    Работа над WorkwearViewModel

--- AccoutingOfWorkwear/ViewModels/StaffViewModel.cs
+++ AccoutingOfWorkwear/ViewModels/StaffViewModel.cs
@@ -1,14 +1,21 @@
 ﻿using AoW.EntityFramework.Date;
 using AoW.EntityFramework.Models;
 using AoW.WPF.ViewModels.Base;
+using MyMVVM.Commands;
 using System.Collections;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AoW.WPF.ViewModels
 {
     internal class StaffViewModel : BaseEntityViewModel<Staff>
     {
+        public StaffViewModel()
+        {
+            SelectWorkwearCommand = new RelayCommand(SelectWorkwearExecute, SelectWorkwearCanExecute);
+        }
+
         protected override async Task<ICollection> Get()
         {
             using (var dbContext = new AowDbContextFactory().CreateDbContext())
@@ -16,5 +23,25 @@ namespace AoW.WPF.ViewModels
                 return dbContext.Staff.ToList();
             }
         }
+        
+        #region Выбор одежды
+
+        // свойство
+        public ICommand SelectWorkwearCommand { get; }    
+
+        // метод
+        private void SelectWorkwearExecute(object obj)
+        {
+            MainViewModel.Navigator.CurrentViewModel = new WorkwearViewModel(this);
+        }
+
+        // проверка
+        private bool SelectWorkwearCanExecute(object obj)
+        {
+            // если есть выделенный элемент
+            return SelectedItem is not null;
+        }
+
+        #endregion
     }
 }
--- AccoutingOfWorkwear/ViewModels/WorkwearViewModel.cs
+++ AccoutingOfWorkwear/ViewModels/WorkwearViewModel.cs
@@ -1,14 +1,49 @@
 ﻿using AoW.EntityFramework.Date;
 using AoW.EntityFramework.Models;
 using AoW.WPF.ViewModels.Base;
+using MyMVVM.Commands;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AoW.WPF.ViewModels
 {
     internal class WorkwearViewModel : BaseEntityViewModel<WorkWear>
     {
+        public WorkwearViewModel(StaffViewModel staffViewModel, Staff staff)
+        {
+            _staffViewModel = staffViewModel;
+            CancelCommand = new RelayCommand(CancelExecute, (obj) => true);
+            _staff = staff; 
+        }
+
+        // StaffViewModel для возращения назад
+        private readonly StaffViewModel _staffViewModel;
+
+        /// <summary>
+        /// Cотрудник, которому выдают СО
+        /// </summary>
+        private readonly Staff _staff;
+
+        /// <summary>
+        /// Информация о поставках
+        /// </summary>
+        private List<ExtraditionInfo> _extraditionInfo;
+
+
+        #region Отмена выдачи одежды
+
+        public ICommand CancelCommand { get; }
+
+        private void CancelExecute(object obj)
+        {
+            MainViewModel.Navigator.CurrentViewModel = _staffViewModel;
+        }
+
+        #endregion
+
         protected override async Task<ICollection> Get()
         {
             using (var dbContext = new AowDbContextFactory().CreateDbContext())
