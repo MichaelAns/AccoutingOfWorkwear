ac417f25a0e862c1264596d0b6a0542f08affaf1
Михаил Щукин <mi035ms@gmail.com>
2022-11-29 15:22:44 +0300

    Создание общего абстрактного класса для ViewModel

--- AccoutingOfWorkwear/ViewModels/WorkwearViewModel.cs
+++ AccoutingOfWorkwear/ViewModels/WorkwearViewModel.cs
@@ -1,20 +1,16 @@
-﻿using AoW.EntityFramework.Date;
-using AoW.EntityFramework.Models;
-using AoW.WPF.ViewModels.Base;
-using System.Collections;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MyMVVM.Navigation.Navigators;
+using MyMVVM.ViewModelBase;
+using System.Windows.Input;
 
 namespace AoW.WPF.ViewModels
 {
-    internal class WorkwearViewModel : BaseEntityViewModel<WorkWear>
-    {        
-        protected override async Task<ICollection> Get()
+    internal class WorkwearViewModel : ViewModel
+    {
+        public ICommand UpdateCurrentViewModelCommand => MainViewModel.Navigator.UpdateCurrentViewModelCommand;
+
+        public WorkwearViewModel()
         {
-            using (var dbContext = new AowDbContextFactory().CreateDbContext())
-            {
-                return dbContext.WorkWear.ToList();
-            }
+                        
         }
     }
 }
